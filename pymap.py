c320a55c4b0ca0305483ee478833b967a2f41e02
Man Fong <mlfong@gatech.edu>
2013-08-26 20:29:45 -0700

    fix follow me icons

--- pymap.py
+++ pymap.py
@@ -6,7 +6,7 @@ first_half = r"""<!DOCTYPE html>
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
-    <title>Simple icons</title>
+    <title>mlfong &#9992;&#9992;&#9992;</title>
     <link href="https://developers.google.com/maps/documentation/javascript/examples/default.css" rel="stylesheet">
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
     <script>
